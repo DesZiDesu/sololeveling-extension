9a106777fabdc137176c5d433a12f8523500ef00
DioneAboveAll <92092632+DesZiDesu@users.noreply.github.com>
2026-08-17 01:07:53 +0700

    Load System notification channel before expansion UI

--- bootstrap.js
+++ bootstrap.js
@@ -1,6 +1,8 @@
 import './index.js';
 import './expansion-foundation.js';
 import './expansion-engine.js';
+import './system-channel-v141.js';
+import './system-unlock-notification-v141.js';
 import './expansion-ui.js';
 import './systems-expansion.js';
 import './system-credit.js';
